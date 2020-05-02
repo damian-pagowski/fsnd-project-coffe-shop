f53e3ac20050c670f86301078cc5324456de832a
Damian Pagowski <d.pagowski@gmail.com>
2020-05-03 01:07:33 +0700

    tested. removed TODO annotations from backend - routes and auth

--- backend/src/api.py
+++ backend/src/api.py
@@ -19,14 +19,7 @@ CORS(app)
 # db_drop_and_create_all()
 
 # ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+
 @app.route('/drinks')
 def get_drinks_short():
     drinks = []
@@ -41,17 +34,9 @@ def get_drinks_short():
         'drinks': drinks
     })
 
-
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
 @app.route('/drinks-detail')
-def get_drinks_details():
+@requires_auth('get:drinks-detail')
+def get_drinks_details(payload):
     drinks = []
     try:
         drinks_data = Drink.query.all()
@@ -63,21 +48,18 @@ def get_drinks_details():
         'drinks': drinks
     })
 
-
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
 @app.route('/drinks', methods=['POST'])
-def create_drink():
+@requires_auth('post:drinks')
+def create_drink(payload):
     body = request.get_json()
     req_title = body.get('title')
     req_recipe = body.get('recipe')
+    if not isinstance(req_recipe, list):
+        abort(422)
+    for each in req_recipe:
+        if not('color' in each and 'parts' in each):
+            abort(422)
+
     try:
         drink = Drink(title=req_title, recipe=json.dumps(req_recipe))
         drink.insert()
@@ -85,20 +67,9 @@ def create_drink():
     except:
         abort(422)
 
-
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
 @app.route('/drinks/<int:id>', methods=['PATCH'])
-def update_drink(id):
+@requires_auth('patch:drinks')
+def update_drink(payload, id):
     body = request.get_json()
     req_title = body.get('title')
     req_recipe = body.get('recipe')
@@ -116,21 +87,12 @@ def update_drink(id):
         else:
             abort(422)
 
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
-@app.route('/drinks/<int:id>', methods = ['DELETE'])
-def delete_drink(id):
-    drink=None
+@app.route('/drinks/<int:id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(payload, id):
+    drink = None
     try:
-        drink=Drink.query.get(id)
+        drink = Drink.query.get(id)
         drink.delete()
         return jsonify({"success": True, "delete": id})
     except:
@@ -138,21 +100,6 @@ def delete_drink(id):
             abort(404)
         else:
             abort(422)
-
-##### FOR TESTING
-@app.route('/')
-@requires_auth(permission='get:drinks-detail')
-def test(payload):
-    print(payload)
-    return jsonify({"success": True})
-
-# delete:drinks
-# get:drinks-detail
-# patch:drinks
-# post:drinks
-##### FOR TESTING
-
-
 
 # Error Handling
 @app.errorhandler(422)
@@ -179,7 +126,6 @@ def unauthorized(error):
         "error": 401,
         "message": "Unauthorized"
     }), 401
-
 
 @app.errorhandler(403)
 def forbidden(error):
