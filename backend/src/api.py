52ae627e04659af085afc0795588f326507fd673
Damian Pagowski <d.pagowski@gmail.com>
2020-05-02 20:48:17 +0700

    removed comments. first implementation of auth - modified code from exercises

--- backend/src/api.py
+++ backend/src/api.py
@@ -141,9 +141,6 @@ def delete_drink(id):
 
 
 # Error Handling
-'''
-Example error handling for unprocessable entity
-'''
 @app.errorhandler(422)
 def unprocessable(error):
     return jsonify({
@@ -178,30 +175,3 @@ def not_found(error):
         "error": 403,
         "message": "Forbidden"
     }), 403
-
-
-'''
-
-
-
-
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False,
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
-
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above
-'''
-
-
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above
-'''
